4a2e8175ecb71f563908fa20f5cccaab7965bc5a
Shokker <rcheiko@student.42nice.fr>
2022-10-08 20:42:04 +0200

    u

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -6,46 +6,48 @@ import { saveImageStore } from './validtypes.decorator';
 
 @Controller('users')
 export class UsersController {
-    constructor(
-        private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService) { }
 
-    @UseGuards(JwtGuard)
-    @Get('infoUser/:user_id')
-    getInfoUser(@Param() params:any): any {
-      return this.usersService.getInfoUser(params.user_id);
-    }
+  @UseGuards(JwtGuard)
+  @Get('infoUser/:user_id')
+  getInfoUser(@Param() params: any): any {
+    return this.usersService.getInfoUser(params.user_id);
+  }
 
-    @UseGuards(JwtGuard)
-    @Get('giveAllUsersPermissions/:user_id')
-    giveAllUsersPermissions(@Param() params:any): any {
-      return this.usersService.giveAllUsersPermissions(params.user_id);
-    }
+  @UseGuards(JwtGuard)
+  @Get('giveAllUsersPermissions/:user_id')
+  giveAllUsersPermissions(@Param() params: any): any {
+    return this.usersService.giveAllUsersPermissions(params.user_id);
+  }
 
-    @UseGuards(JwtGuard)
-    @Get('giveUsersTweet/:user_id')
-    giveUsersTweet(@Param() params:any): any {
-      return this.usersService.giveUsersTweet(params.user_id);
-    }
+  @UseGuards(JwtGuard)
+  @Get('giveUsersTweet/:user_id')
+  giveUsersTweet(@Param() params: any): any {
+    return this.usersService.giveUsersTweet(params.user_id);
+  }
 
-    @UseGuards(JwtGuard)
-    @Post('tweet/:user_id')
-    tweet(@Param() params:any, @Body() body:any): any {
-      return this.usersService.tweet(body.tweet, params.user_id, body.media);
-    }
+  @UseGuards(JwtGuard)
+  @Post('tweet/:user_id')
+  tweet(@Param() params: any, @Body() body: any): any {
+    return this.usersService.tweet(body.tweet, params.user_id, body.media);
+  }
 
-    @UseGuards(JwtGuard)
-    @Post('tweetPermission/:user_id_owner')
-    tweetPermission(@Param() params:any, @Body() body:any): any {
-      return this.usersService.tweetPermission(body.tweet, params.user_id_owner, body.user_id, body.gif);
-    }
+  @UseGuards(JwtGuard)
+  @Post('tweetPermission/:user_id_owner')
+  tweetPermission(@Param() params: any, @Body() body: any): any {
+    return this.usersService.tweetPermission(body.tweet, params.user_id_owner, body.user_id, body.gif);
+  }
 
-    @Post('file')
-    @UseInterceptors(AnyFilesInterceptor(saveImageStore))
-    async test(@UploadedFiles() files: Array<Express.Multer.File>) {
-      console.log('files :', files);
-      if (!files[0]?.filename) {
-          console.log('File must be a png/jpg/jpeg/gif/mp4');
-        }
-      return 'File has been uploaded';
+  @UseGuards(JwtGuard)
+  @Post('file/:user_id')
+  @UseInterceptors(AnyFilesInterceptor(saveImageStore))
+  async tweetWithFiles(@UploadedFiles() files: Array<Express.Multer.File>, @Body() body: any) {
+    console.log('files :', files);
+    console.log('body :', body);
+    if (!files[0]?.filename) {
+      console.log('File must be a png/jpg/jpeg/gif/mp4');
     }
+    return 'File has been uploaded';
+  }
 }
